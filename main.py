fab4a6113b7756bb87d422759fd60968a283070b
Mohammad Yameen <yameen@coredge.io>
2023-02-09 14:11:57 +0530

    11

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+###############################################################################
+# Copyright (c) 2022-present CorEdge India Pvt. Ltd - All Rights Reserved     #
+# Unauthorized copying of this file, via any medium is strictly prohibited    #
+# Proprietary and confidential                                                #
+# Written by Shubham Kumar <shubhamkumar@coredge.io>, Feb 2023                #
+###############################################################################
 
-print('HEllo')
+def func():
+    pass
